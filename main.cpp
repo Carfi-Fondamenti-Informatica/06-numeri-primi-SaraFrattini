45e25cf62ee38d325b7c4246ad3c0c6f2266b485
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-12-03 00:16:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int p;
+    cin >> p;
+    if (SCIAOBELLO(p) == 0) {
+        cout << "numero primo" << endl;
+    }
+    else {
+        cout << "numero non primo" << endl;
+    }
+    return 0;
 }
