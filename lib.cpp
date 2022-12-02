3144ac5e1ecb0641c006157d488cd4e45f886a14
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-12-02 23:03:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,8 @@
-
+#include "libreria1.h"
+#include <iostream>
+using namespace std;
+bool SCIAOBELLO (int i, int x){
+    cout <<i<< endl;
+    cout <<x/i<< endl;
+    return x/i==0 ? x==i : SCIAOBELLO (i+1,x);
+}
